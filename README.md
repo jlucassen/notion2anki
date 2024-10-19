037e79629651ce080046ce96bd867c282359ed0a
James <james.z.lucassen@gmail.com>
2024-10-19 17:36:09 -0400

    changed format

--- README.md
+++ README.md
@@ -1,11 +1,4 @@
 # notion2anki
 
-Now supports two formats!
-- One-deep bullets are turned into cards
-- Top level bullets are discarded if they have sub-bullets underneath and sub-bullets become bulleted notes
-
-All notes must be clozes, with {{just brackets}}, cloze numbering is filled in automatically
-
-The Notion page title is parsed as the deck title, and h3 headers are parsed as tags. All cards are tagged with "deck:tag" based on the most recent h3.
-
-When importing into anki choose pipe field separators, cloze notes, the right deck, and set the tag field to be tags.
+New format. Everything on one page, nested headers become nested tags. 
+Removed sub-bullets for now, might add back if I need them.
--- notion2anki.py
+++ notion2anki.py
@@ -3,12 +3,23 @@ import os
 import zipfile
 import re
 
-def number_clozes(block):
+def split_by_header(contents, level):
+    split = '\n' + '#' * level + ' '
+    groups = contents.split(split)
+    out = {}
+    for group in groups:
+        if len(group) == 0: continue
+        tag = group.split("\n")[0]
+        body = '\n'+'\n'.join(group.split("\n")[1:])
+        out[tag] = body
+    return out
+
+def number_clozes(card):
     i=1
-    while re.search("{{(?!(c\d))", block):
-        block = re.sub("{{(?!(c\d))", "{{c"+str(i)+"::", block, count=1)
+    while re.search("{{(?!(c\d))", card):
+        card = re.sub("{{(?!(c\d))", "{{c"+str(i)+"::", card, count=1)
         i += 1
-    return block
+    return card
 
 def main(path):
     if path is None:
@@ -22,29 +33,50 @@ def main(path):
     for path in paths:
         with open(path, "r") as f:
             contents = f.read()
-        title = contents.split("\n")[0][2:]
-        contents = '\n'.join([x for x in contents.split("\n")[1:] if len(x) > 0]) # remove deck title and empty lines
-        outfile = path[:-2] + "txt"
-        tag = None
-        with open(outfile, "w") as f:
-            tag_groups = contents.split("### ")
-            for tag_group in tag_groups:
-                if not tag_group.startswith('- '): # no bullet at start, must be a tag
-                    tag = tag_group.split("\n- ")[0]
-                    bullets = tag_group.split('\n- ')[1:] # remove tag from bullets
-                else:
-                    bullets = tag_group.split('\n- ')
-                for bullet in bullets:
-                    if "    - " in bullet: # two deep detected, remove outer bullets and remove excess spaces from inner bullets
-                        card = '<br>'.join([line for line in bullet.split('\n') if "    - " in line])
-                        card = card.replace("    ", '')
-                    else: # just one deep, remove bullets
-                        card = bullet.replace('  - ', '')
-                    if card.endswith("\n"): card = card[:-1] # remove ending \n
-                    card = number_clozes(card)
-                    if tag is not None:
-                        card = card + f"|{title.lower().replace(' ','')}:{tag.lower().replace(' ', '')}"
-                    f.write(card+'\n')                  
+        contents = '\n'.join(contents.split("\n")[1:]) # remove title line
+        groups_1 = split_by_header(contents, 1)
+        with open(path[:-3]+'.txt', 'w') as f:
+            for tag1, group1 in groups_1.items():
+                groups_2 = split_by_header(group1, 2)
+                for tag2, group2 in groups_2.items():
+                    groups_3 = split_by_header(group2, 3)
+                    for tag3, group3 in groups_3.items():
+                        tag = tag1 + ((':'+tag2) if len(tag2) > 0 else '') + ((':'+tag3) if len(tag3) > 0 else '')
+                        cards = group3.split("\n- ")
+                        for card in cards:
+                            card = card.strip()
+                            if len(card) == 0: continue # empty card
+                            card.replace('\n', '<br>')
+                            card = number_clozes(card)
+                            card = card + f"|{tag}"
+                            f.write(card+'\n')      
+
+
+
+
+
+
+
+
+
+            # tag_groups = contents.split("### ")
+            # for tag_group in tag_groups:
+            #     if not tag_group.startswith('- '): # no bullet at start, must be a tag
+            #         tag = tag_group.split("\n- ")[0]
+            #         bullets = tag_group.split('\n- ')[1:] # remove tag from bullets
+            #     else:
+            #         bullets = tag_group.split('\n- ')
+            #     for bullet in bullets:
+            #         if "    - " in bullet: # two deep detected, remove outer bullets and remove excess spaces from inner bullets
+            #             card = '<br>'.join([line for line in bullet.split('\n') if "    - " in line])
+            #             card = card.replace("    ", '')
+            #         else: # just one deep, remove bullets
+            #             card = bullet.replace('  - ', '')
+            #         if card.endswith("\n"): card = card[:-1] # remove ending \n
+            #         card = number_clozes(card)
+            #         if tag is not None:
+            #             card = card + f"|{title.lower().replace(' ','')}:{tag.lower().replace(' ', '')}"
+            #         f.write(card+'\n')                  
 
                     
 
