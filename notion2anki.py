3a157f2c511db1f71f813471ca118825a4f759ae
James <james.z.lucassen@gmail.com>
2024-10-28 22:23:15 -0400

    silly bug

--- notion2anki.py
+++ notion2anki.py
@@ -2,6 +2,7 @@ import argparse
 import os
 import zipfile
 import re
+from collections import Counter
 
 def split_by_header(contents, level):
     split = '\n' + '#' * level + ' '
@@ -23,6 +24,21 @@ def number_clozes(card):
         raise ValueError(f"No cloze found in card:\n\n{card}")
     return card
 
+def check_card_template(card):
+    card = card.lower()
+    if re.search(r"to fix {{.*?, {{", card):
+        return "to_fix_X_Y"
+    if re.search(r"the (argument|args|syntax) for .*? (is|are) {{", card):
+        return "args/syntax"
+    if re.search(r"to ({{)?.*?(}})?,( do not)? (use )?{{", card):
+        return "to_X_use_Y"
+    if re.search(r"how .*? works: {{", card):
+        return "how_X_works"
+    if re.search(r".*? (is|are) {{", card):
+        return "X_is_Y"
+    print(f"No template found for card: {card}")
+
+
 def main(path):
     if path is None:
         for file in os.listdir():
@@ -37,6 +53,7 @@ def main(path):
             contents = f.read()
         contents = '\n'.join(contents.split("\n")[1:]) # remove title line
         groups_1 = split_by_header(contents, 1)
+        card_templates = Counter()
         with open(path[:-3]+'.txt', 'w') as f:
             for tag1, group1 in groups_1.items():
                 groups_2 = split_by_header(group1, 2)
@@ -48,10 +65,16 @@ def main(path):
                         for card in cards:
                             card = card.strip()
                             if len(card) == 0: continue # empty card
-                            card.replace('\n', '<br>')
+                            card_templates[check_card_template(card)] += 1
+                            card = card.replace('\n', '<br>')
                             card = number_clozes(card)
                             card = card + f"\t{tag}"
-                            f.write(card+'\n')      
+                            f.write(card+'\n')   
+                            if '<br>' in card:
+                                print(f"Card with <br> found: {card}")
+        print(f"Finished {path}. Card templates found:")
+        for p in sorted(card_templates.items(), key=lambda x:x[1], reverse=True):
+            print(f"{p[0]}: {p[1]}")
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
